a9e0f31665cdfc650b95f0a350f607255bbc2e5f
LesGrooper <eqimulia@ymail.com>
2026-05-07 20:14:09 +0700

    nambahin app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import AppRoutes from './routes/AppRoutes';
 
